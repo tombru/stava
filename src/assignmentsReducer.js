40ac3aaad262d713b01cf48271c3187819c334ad
Tomas Bruhn <tomas.bruhn@comhem.com>
2017-07-19 19:26:15 +0200

    -

--- src/assignmentsReducer.js
+++ src/assignmentsReducer.js
@@ -1,6 +1,6 @@
 import { APPROVE_ANSWER, SHOW_ASSIGNMENT, UPDATE_ANSWER } from "./assignmentsActionTypes";
 
-const assignments = ["vad gör pojken - pojken gråter.jpg","vad gör pojken - pojken skrattar.jpg", "vad gör flickan-hon borstar tänderna.jpg", "vad gör pojken-han springer.gif", "vad gör flickan-hon äter banan.jpg", "vad gör bill callahan-han spelar gitarr.jpg"];
+const assignments = ["vad gör pojken - han gråter.jpg","vad gör pojken - han skrattar.jpg", "vad gör flickan-hon borstar tänderna.jpg", "vad gör pojken-han springer.gif", "vad gör flickan-hon äter banan.jpg", "vad gör bill callahan-han spelar gitarr.jpg"];
 
 const pickRandom = (a) => {
     return a[Math.floor(Math.random() * a.length)] || "";
