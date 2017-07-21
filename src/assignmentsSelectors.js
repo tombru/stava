d3d931031bafa4c219bf09bf7ab3826a08da4e78
Tomas Bruhn <tomas.bruhn@comhem.com>
2017-07-21 19:01:32 +0200

    refactoring

--- src/assignmentsSelectors.js
+++ src/assignmentsSelectors.js
@@ -1,15 +1,13 @@
 import { createSelector } from 'reselect'
 
-const getAssignments = (state) => state.assignments;
+const getAssignments = state => state.assignments;
 
-const targetText = (currentFilename) => {
+const targetText = currentFilename => {
     const part = currentFilename.substring(currentFilename.indexOf("-") + 1, currentFilename.length);
     return part.substring(0, part.indexOf(".")).replace("_", " ").trim();
 };
 
-const question = (currentFilename) => {
-    return currentFilename.substring(0, currentFilename.indexOf("-")).trim();
-};
+const question = currentFilename => currentFilename.substring(0, currentFilename.indexOf("-")).trim()
 
 export const currentTextSelector = createSelector(
     getAssignments,
