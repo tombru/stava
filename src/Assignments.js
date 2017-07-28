0ca87f4ca05149ffc3668507914efb3eb09beb0a
Tomas Bruhn <tomas.bruhn@comhem.com>
2017-07-28 15:48:26 +0200

    refactoring

--- src/Assignments.js
+++ src/Assignments.js
@@ -3,27 +3,26 @@ import Assignment from "./Assignment"
 
 function Assignments({ currentAssignment, currentText, showAssignment, previousText, onChange }) {
     return (
-        currentAssignment ? <div className="container">
+        <div className="container">
             {showAssignment &&
-            <span>
-                    <span>
+                <span>
+                    {currentAssignment &&
                         <Assignment
-                            question={currentAssignment.question}
-                            currentText={currentText}
-                            link={currentAssignment.link}
-                            onChange={onChange}
+                        question={currentAssignment.question}
+                        currentText={currentText}
+                        link={currentAssignment.link}
+                        onChange={onChange}
                         />
-                    </span>
+                    }
                 </span>
             }
             {!showAssignment &&
-            <div className="previousAssignment">
-                <h1>{previousText}</h1>
-            </div>
+                <div className="previousAssignment">
+                    <h1>{previousText}</h1>
+                </div>
             }
-        </div>: null
+        </div>
     )
-
 }
 
 export default Assignments;
